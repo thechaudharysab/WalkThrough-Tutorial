2cb2b215e28a5b770342601a2275867c2164aeab
Chaudhry Talha <gameral@live.com>
2019-11-03 00:48:21 +0500

    FinishedDemo

--- WalkThrough/WalkThrough/ScreenOneViewController.swift
+++ WalkThrough/WalkThrough/ScreenOneViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Lottie
 
 class ScreenOneViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
@@ -16,6 +17,10 @@ class ScreenOneViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     @IBOutlet weak var boyGirlSegment: UISegmentedControl!
     @IBOutlet weak var spinButton: UIButton!
     
+    let girlSegmentTap = AnimationView(name: "tap")
+    let boySegmentTap = AnimationView(name: "tap")
+    let spinButtonTap = AnimationView(name: "point")
+    
     var boyNames: [String] = ["Liam","Noah","William","James","Oliver","Benjamin","Elijah","Lucas","Mason","Logan"]
     var girlNames: [String] = ["Emma","Olivia","Ava","Isabella","Sophia","Charlotte","Mia","Amelia","Arya","Sansa"]
     
@@ -24,17 +29,108 @@ class ScreenOneViewController: UIViewController, UIPickerViewDelegate, UIPickerV
         
         namePicker.delegate = self
         namePicker.dataSource = self
-
+        UserDefaults.standard.removeObject(forKey: "NeedWalkThrough")
+        startWalkThrough()
+        //Displaying popup to ask if user need walkthorugh
+        
         // Do any additional setup after loading the view.
     }
     
+//    func showAlert() {
+//
+//
+////        if UserDefaults.standard.bool(forKey: "NeedWalkThrough") == true {
+////
+////        } else if UserDefaults.standard.bool(forKey: "NeedWalkThrough") == false {
+////
+////        } else {
+//
+//            let alert = UIAlertController(title: "Welcome!", message: "Let's Get Started. Do you need a quick tutorial of how to use this app?", preferredStyle: UIAlertController.Style.alert)
+//
+//            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (actionAlert) in
+//                self.startWalkThrough()
+//                UserDefaults.standard.set(false, forKey: "NeedWalkThrough")
+//            }))
+//            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { (actionAlert) in
+//                UserDefaults.standard.set(true, forKey: "NeedWalkThrough")
+//            }))
+//
+//            alert.addAction(UIAlertAction(title: "Don't show this again", style: .destructive, handler: { (actionAlert) in
+//                UserDefaults.standard.set(false, forKey: "NeedWalkThrough")
+//            }))
+//
+//            self.present(alert, animated: true, completion: nil)
+//
+//   //     }
+//
+//    }
+    
+    func startWalkThrough()
+    {
+        girlSegmentTap.frame = CGRect(x: 0, y: 0, width: 65, height: 60)
+        girlSegmentTap.center =  CGPoint(x: self.view.center.x+80, y: boyGirlSegment.center.y) //numberRaiseButton.center
+        girlSegmentTap.loopMode = .loop
+        girlSegmentTap.animationSpeed = 0.5
+        girlSegmentTap.contentMode = .scaleAspectFill
+        self.view.addSubview(girlSegmentTap)
+        self.view.bringSubviewToFront(girlSegmentTap)
+        let girlSegmentTapGesture = UITapGestureRecognizer(target: self, action: #selector(girlSegmentTapDone(sender:)))
+        girlSegmentTap.addGestureRecognizer(girlSegmentTapGesture)
+        girlSegmentTap.play(fromFrame: 0, toFrame: 8, loopMode: .loop, completion: nil)
+    }
+    
+    @objc func girlSegmentTapDone(sender:UITapGestureRecognizer){
+       
+        boyGirlSegment.selectedSegmentIndex = 1
+        namePicker.reloadAllComponents()
+        girlSegmentTap.isHidden = true
+        
+        boySegmentTap.frame = CGRect(x: 0, y: 0, width: 65, height: 60)
+        boySegmentTap.center =  CGPoint(x: self.view.center.x-40, y: boyGirlSegment.center.y) //numberRaiseButton.center
+        boySegmentTap.loopMode = .loop
+        boySegmentTap.animationSpeed = 0.5
+        boySegmentTap.contentMode = .scaleAspectFill
+        self.view.addSubview(boySegmentTap)
+        self.view.bringSubviewToFront(boySegmentTap)
+        let boySegmentTapGesture = UITapGestureRecognizer(target: self, action: #selector(boySegmentTapDone(sender:)))
+        boySegmentTap.addGestureRecognizer(boySegmentTapGesture)
+        boySegmentTap.play(fromFrame: 0, toFrame: 8, loopMode: .loop, completion: nil)
+        
+    }
+    
+    @objc func boySegmentTapDone(sender:UITapGestureRecognizer){
+        
+        boyGirlSegment.selectedSegmentIndex = 0
+        namePicker.reloadAllComponents()
+        boySegmentTap.isHidden = true
+        
+        spinButtonTap.frame = CGRect(x: 0, y: 0, width: 70, height: 70)
+        spinButtonTap.center =  CGPoint(x: self.view.center.x, y: spinButton.center.y+60) //numberRaiseButton.center
+        spinButtonTap.loopMode = .loop
+        spinButtonTap.animationSpeed = 1.0
+        spinButtonTap.contentMode = .scaleAspectFill
+        self.view.addSubview(spinButtonTap)
+        self.view.bringSubviewToFront(spinButtonTap)
+        let spinButtonTapGesture = UITapGestureRecognizer(target: self, action: #selector(spinButtonTapDone(sender:)))
+        spinButtonTap.addGestureRecognizer(spinButtonTapGesture)
+        spinButtonTap.play(fromFrame: 0, toFrame: 41, loopMode: .loop, completion: nil)
+        
+    }
+    
+    @objc func spinButtonTapDone(sender:UITapGestureRecognizer) {
+        
+        spinButtonTap.isHidden = true
+        print("Walk Through Finished")
+    }
+    
     @IBAction func boyBirlSegmentChanged(_ sender: Any) {
+        
         namePicker.reloadAllComponents()
     }
     
     
     @IBAction func spinButtonPressed(_ sender: Any) {
-        
+        spinButtonTap.isHidden = true
         let randomInt = Int.random(in: 0 ... 10)
         namePicker.selectRow(randomInt, inComponent: 0, animated: true)
         
